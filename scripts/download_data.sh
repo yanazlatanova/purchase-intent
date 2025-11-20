2504bdd14dcffd556e59e4597a5e5ddfdd888f9c
notPlancha <31565873+notPlancha@users.noreply.github.com>
2025-11-20 17:42:07 +0100

    download data

---
+++ scripts/download_data.sh
@@ -0,0 +1,3 @@
+curl -o data/yello_tripdata_2025-01.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet
+curl -o data/yello_tripdata_2025-02.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-02.parquet
+curl -o data/yello_tripdata_2025-03.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-03.parquet
