2ff8fbe92ab5338f4bc3c2a1407563bb77e37519
notPlancha <31565873+notPlancha@users.noreply.github.com>
2025-11-24 15:53:49 +0100

    before changing dataset

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,3 +1,27 @@
-curl -o data/yello_tripdata_2025-01.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet
-curl -o data/yello_tripdata_2025-02.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-02.parquet
-curl -o data/yello_tripdata_2025-03.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-03.parquet
+
+# Create data directory if it doesn't exist
+mkdir -p data
+
+# Download yellow taxi data for 2025-01 if it doesn't exist
+if [ ! -f "data/yellow_tripdata_2025-01.parquet" ]; then
+    echo "Downloading yellow taxi data for 2025-01..."
+    curl -o data/yellow_tripdata_2025-01.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet
+else
+    echo "File data/yellow_tripdata_2025-01.parquet already exists, skipping download."
+fi
+
+# Download yellow taxi data for 2025-02 if it doesn't exist
+if [ ! -f "data/yellow_tripdata_2025-02.parquet" ]; then
+    echo "Downloading yellow taxi data for 2025-02..."
+    curl -o data/yellow_tripdata_2025-02.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-02.parquet
+else
+    echo "File data/yellow_tripdata_2025-02.parquet already exists, skipping download."
+fi
+
+# Download yellow taxi data for 2025-03 if it doesn't exist
+if [ ! -f "data/yellow_tripdata_2025-03.parquet" ]; then
+    echo "Downloading yellow taxi data for 2025-03..."
+    curl -o data/yellow_tripdata_2025-03.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-03.parquet
+else
+    echo "File data/yellow_tripdata_2025-03.parquet already exists, skipping download."
+fi
